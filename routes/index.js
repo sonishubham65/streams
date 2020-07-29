28794c4ea03d1d6f5a77f472236858dd11b10313
subhamsoni <sonishubham65@gmail.com>
2020-07-29 13:18:56 +0530

    changed the format of array buffer

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var { createReadStream, ReadStream } = require('fs');
-/* GET home page. */
-
-// let buffer = Buffer.from([]);
-// buffer = Buffer.concat([buffer, Buffer.from("hihu")])
-// console.log(buffer.length)
 
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 })
 router.post('/', function (req, res, next) {
-  //res.render('index', { title: 'Express' });
-  console.log(req.body.start)
   let readStream = createReadStream('./public/mov.mp4', { start: parseInt(req.body.start), end: parseInt(req.body.end) });
 
-  let buffer = Buffer.from("");
+  let buffer = Buffer.alloc(0);
   readStream.on('open', () => {
-    console.log('Stream opened...');
+    console.log('Stream opened.');
   });
   readStream.on('data', (chunk) => {
     buffer = Buffer.concat([buffer, chunk])
   });
   readStream.on('end', () => {
-    console.log('Stream Closed...', buffer.length);
+    console.log('Stream Closed.', buffer.length);
     if (buffer.length)
-      res.json({ success: 1, data: buffer.toJSON() })
+      res.end(buffer)
     else
       res.json({ success: 0 })
   });
